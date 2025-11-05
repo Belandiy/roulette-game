9e03dfa39539e56c60a63be50469d78656bf6625
malliedo <malliedo@student.21-school.ru>
2025-11-05 15:48:52 +0500

    create db.py and modify schema.sql

---
+++ db.py
@@ -0,0 +1,18 @@
+import sqlite3
+from flask import g
+
+DATABASE = "database.db"
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(DATABASE)
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+
+def init_db():
+    db = sqlite3.connect(DATABASE)
+    with open("schema.sql", "r", encoding="utf-8") as f:
+        db.executescript(f.read())
+    db.close()
