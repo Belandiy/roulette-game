f9c1c2b857afe4ea937e4506e3d3125b990b50b7
Lomikk <92258604+Lomikk@users.noreply.github.com>
2025-12-04 12:07:38 +0500

    Revert "remove unnecessary space app.py"
    This reverts commit fe048ee6a74221d50eb5d6b287a99ef329c543dc.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify, session
 import random
+
 import db
+
 
 app = Flask(__name__)
 app.secret_key = 'dev_key_sprint_2'  # Временный ключ для сессий
@@ -8,19 +10,77 @@ app.secret_key = 'dev_key_sprint_2'  # Временный ключ для сес
 
 @app.route("/")
 def home():
+
     return render_template("index.html")
 
 @app.route("/rules")
 def rules():
     return render_template("rules.html")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.route("/api/spin", methods=["POST"])
 def api_spin():
     """
+
+
     Request JSON:
       { "nickname": "Player1" }  # без поля bet
     Response JSON:
       { "nickname":"Player1", "result":[1,2,3], "score":0, "combo":"none" }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     """
     data = request.get_json(silent=True) or {}
     nickname = data.get("nickname", "anonymous")
@@ -28,7 +88,15 @@ def api_spin():
     # Генерация результата (серверная сторона — честно)
     result = [random.randint(0, 9) for _ in range(3)]
 
+
+
     # Простая логика очков без ставки
+
+
+
+
+
+
     if result[0] == result[1] == result[2]:
         combo = "three_of_kind"
         score = 100
@@ -38,24 +106,80 @@ def api_spin():
     else:
         combo = "none"
         score = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return jsonify({
 
         "nickname": nickname,
         "result": result,
         "score": score,
         "combo": combo
+
+
     }), 200
 
 @app.route("/api/leaderboard")
 def api_leaderboard():
     """
     Заглушка для турнирной таблицы
+
+
+
+
+
+
+
     """
     # Возвращаем фиктивные данные
     leaderboard = [
         {"nickname": "Player1", "best_score": 100},
         {"nickname": "Player2", "best_score": 50},
         {"nickname": "Player3", "best_score": 20}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     ]
 
     return jsonify(leaderboard), 200
